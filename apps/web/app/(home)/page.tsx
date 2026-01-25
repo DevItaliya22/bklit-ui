5b910cb35ab6ab10f9f405c45c38f23883aeeadd
Matt <mattsince87@gmail.com>
2026-01-25 17:52:11 +0200

    Theming

--- apps/web/app/(home)/page.tsx
+++ apps/web/app/(home)/page.tsx
@@ -1,14 +1,19 @@
 import Link from "next/link";
 import { HomeComponents } from "@/components/home-components";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col items-center justify-center space-y-24 px-4 py-24 text-center">
-      <div className="max-w-xl">
-        <h1 className="mb-6 font-bold text-2xl sm:text-4xl">Bklit UI</h1>
+      <div className="max-w-xl space-y-6">
+        <div className="mx-auto flex w-fit items-center justify-center rounded-full border p-px">
+          <Badge variant="secondary">Version</Badge>
+          <Badge variant="ghost">Pre-release</Badge>
+        </div>
+        <h1 className="font-bold text-2xl sm:text-4xl">Bklit UI</h1>
 
-        <p className="mb-10 text-lg sm:text-xl">
+        <p className="text-lg sm:text-xl">
           A collection of Open Source charts and utility components that you can
           customize and extend.
         </p>
